def433a8b1f81e03d56050c4edb077dfd7769b2f
TomMonks <thomas.m.monks@gmail.com>
2024-07-22 16:11:25 +0100

    feat(single_run): modified so that do not need to create simmer env outside of func

--- main.R
+++ main.R
@@ -4,7 +4,7 @@ library(treat.sim)
 default_exp <- create_experiment(log_level=0)
 
 # run 10 replications of the experiment 
-envs <- multiple_replications(default_exp, n_reps=10)
+envs <- multiple_replications(default_exp, n_reps=50)
 
 # process simmer environments into simple data.frame of reps by KPIs
 rep_table <- replication_results_table(envs, default_exp, 60 * 19)
